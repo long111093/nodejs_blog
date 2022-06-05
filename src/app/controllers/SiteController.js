f74775a4032565797e89e8bd98642fdede4916fa
= <dangvanlong26081993@gmail.com>
2022-06-05 10:18:28 +0700

    Refactor: Create routes

---
+++ src/app/controllers/SiteController.js
@@ -0,0 +1,13 @@
+class SiteController{
+    // [GET] / news
+    index(req, res){
+        res.render('home')
+    }
+
+    //[GET] /search/:slug
+    search(req, res) {
+        res.render('search')
+    }
+}
+
+module.exports = new SiteController;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const route = require('./routes')
 const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
@@ -19,14 +20,8 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, '/resources/views'));
 console.log("__dirname",__dirname);
 
-app.get('/', (req, res)=>{
-    res.render('home');
-})
-app.get('/news', (req, res)=>{
-    res.render('news');
-})
-app.get('/search', (req, res)=>{
-    res.render('search');
-})
+
+// Route init
+route(app)
 
 app.listen(port, ()=> console.log(`Express app listening at http://localhost:${port}`))
